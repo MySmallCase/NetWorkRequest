73c731131e6376886482c898ac3fd9651c6335a9
向云飞 <978771447@qq.com>
2016-01-07 15:16:59 +0800

    创建工程

---
+++ NetWorkRequest/NetWorkRequest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetWorkRequest
+//
+//  Created by MyMac on 16/1/7.
+//  Copyright © 2016年 MyMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
